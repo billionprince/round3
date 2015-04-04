80b0595bc4372e7493cf25ee211cad11dcd1debf
HC <billionprince@gmail.com>
2015-04-04 23:21:29 +0800

    several functions of itemHandler and userHandler

--- util/itemHandler.py
+++ util/itemHandler.py
@@ -1,33 +1,53 @@
-from util import fileHandler
+# -*- coding: utf-8 -*-
+import sqlite3
 import settings
+import os
 
-def item_input():
-    lines = fileHandler.readFile(settings.ITEM_FILE)
-    return ({}, {}, {})
-#     item_dict = {}
-#     item_type_cnt = {}
-#     item_type_dict = {}
-#     item_type_idx = {}
-# # item_id,item_geohash,item_category
-#     for line in lines:
-#         item_dict[line['item_id']] = [c[1], c[2]]
-#         if c[2] not in item_type_cnt:
-#             item_type_cnt[c[2]] = 1
-#             item_type_dict[c[2]] = len(item_type_cnt)
-#             item_type_idx[len(item_type_cnt)] = c[2]
-#         else:
-#             item_type_cnt[c[2]] += 1
-#
-#     return item_dict, item_type_dict, item_type_idx
+DB_PATH = os.path.join(os.path.dirname(settings.__file__), settings.DB_NAME)
+CONN = sqlite3.connect(DB_PATH)
+USERLIST = ['user_id', 'item_id', 'behavior_type', 'user_geohash', 'item_category', 'time']
+IMTELIST = ['item_id', 'item_geohash', 'item_category']
+#包括浏览、收藏、加购物车、购买，对应取值分别是1、2、3、4
 
-def get_item_type():
-    return []
+def get_all_itemid():
+    cursor = CONN.cursor()
+    cursor.execute('select distinct item_id from itemlist')
+    lines = cursor.fetchall()
+    rec = [str(line[0]) for line in lines]
+    cursor.close()
+    return rec
 
-def get_items(lineNum=None):
-    lines = []
-    if lineNum:
-        lines = fileHandler.readFile(settings.ITEM_FILE, lineNum)
-    else:
-        lines = fileHandler.readFile(settings.ITEM_FILE, lineNum)
-    return lines
+def get_item_type_by_itemid(tid):
+    cursor = CONN.cursor()
+    int_tid = tid
+    if not isinstance(tid, int):
+        int_tid = int(int_tid)
+    cursor.execute('select distinct item_category from itemlist where item_id=%s' % int_tid)
+    line = cursor.fetchone()
+    rec = {tid: str(line[0])}
+    cursor.close()
+    return rec
 
+def get_item_by_item_type(item_type):
+    cursor = CONN.cursor()
+    if not isinstance(item_type, int):
+        int_item_type = int(item_type)
+    cursor.execute('select distinct item_id from itemlist where item_category=%s' % int_item_type)
+    lines = cursor.fetchall()
+    rec = {item_type: [str(line[0]) for line in lines]}
+    cursor.close()
+    return rec
+
+def get_item_buytimes_buy_itemid(tid):
+    cursor = CONN.cursor()
+    if not isinstance(tid, int):
+        int_tid = int(tid)
+    q = 'select item_id, count(*) as item_buy_time from userlist '
+    q += 'where userlist.behavior_type=4 '
+    q += 'group by item_id '
+    q += 'order by item_buy_time desc'
+    cursor.execute(q)
+    lines = cursor.fetchall()
+    rec = {tid: [(str(line[0]), line[1]) for line in lines]}
+    cursor.close()
+    return rec[tid][:10]
--- util/userHandler.py
+++ util/userHandler.py
@@ -9,6 +9,13 @@ USERLIST = ['user_id', 'item_id', 'behavior_type', 'user_geohash', 'item_categor
 IMTELIST = ['item_id', 'item_geohash', 'item_category']
 #包括浏览、收藏、加购物车、购买，对应取值分别是1、2、3、4
 
+def get_all_uid():
+    cursor = CONN.cursor()
+    cursor.execute('select distinct user_id from userlist')
+    lines = cursor.fetchall()
+    rec = [line[0] for line in lines]
+    cursor.close()
+    return rec
 
 def get_user_data_set_by_time(percent):
     cursor = CONN.cursor()
@@ -18,7 +25,13 @@ def get_user_data_set_by_time(percent):
         require = int(total * percent)
     else:
         raise ValueError('percent should be float type')
-    cursor.execute('select * from userlist order by time limit %s' % require)
+    str = 'select * from userlist '
+    if percent < 0:
+        str += 'order by time desc '
+    else:
+        str += 'order by time asc '
+    str += 'limit %s' % abs(require)
+    cursor.execute(str)
     lines = cursor.fetchall()
     rec = [{field: line[idx] for idx, field in enumerate(USERLIST)} for line in lines]
     cursor.close()
@@ -42,32 +55,30 @@ def get_user_buy_item_by_userid(uid):
     int_uid = uid
     if not isinstance(uid, int):
         int_uid = int(int_uid)
-    cursor.execute('select distinct item_id from userlist where user_id=%s' % int_uid)
+    cursor.execute('select distinct item_id from userlist where user_id=%s and behavior_type=4' % int_uid)
     lines = cursor.fetchall()
     rec = {uid: [str(line[0]) for line in lines]}
     cursor.close()
     return rec
 
-def get_item_type_by_itemid(tid):
+def get_user_buy_item_and_buytimes_by_userid(uid):
     cursor = CONN.cursor()
-    int_tid = tid
-    if not isinstance(tid, int):
-        int_tid = int(int_tid)
-    cursor.execute('select distinct item_category from itemlist where item_id=%s' % int_tid)
-    line = cursor.fetchone()
-    rec = {tid: str(line[0])}
-    cursor.close()
-    return rec
-
-def get_item_by_item_type(item_type):
-    cursor = CONN.cursor()
-    if not isinstance(item_type, int):
-        int_item_type = int(item_type)
-    cursor.execute('select distinct item_id from itemlist where item_category=%s' % int_item_type)
+    int_uid = uid
+    if not isinstance(uid, int):
+        int_uid = int(int_uid)
+    cursor.execute('select item_id, count(*) from userlist where user_id=%s and behavior_type=4 group by item_id' % int_uid)
     lines = cursor.fetchall()
-    rec = {item_type: [str(line[0]) for line in lines]}
+    rec = {uid: [(str(line[0]), int(line[1])) for line in lines]}
     cursor.close()
     return rec
 
-if __name__ == '__main__':
-    print get_item_by_item_type('7143')
+def get_items_on_shopping_cart_by_uid(uid):
+    cursor = CONN.cursor()
+    int_uid = uid
+    if not isinstance(uid, int):
+        int_uid = int(int_uid)
+    cursor.execute('select distinct item_id from userlist where user_id=%s and behavior_type=3' % int_uid)
+    lines = cursor.fetchall()
+    rec = {uid: [str(line[0]) for line in lines]}
+    cursor.close()
+    return rec
