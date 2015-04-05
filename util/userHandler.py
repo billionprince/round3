1ca5b4fda689e0da6417fdcad90eb496153628b9
Gilhirith <gilhirith@gmail.com>
2015-04-05 10:31:53 +0800

    add some functions

--- util/userHandler.py
+++ util/userHandler.py
@@ -57,7 +57,7 @@ def get_user_buy_items_by_userid(uid):
         int_uid = int(int_uid)
     cursor.execute('select distinct item_id from userlist where user_id=%s and behavior_type=4' % int_uid)
     lines = cursor.fetchall()
-    rec = {uid: [str(line[0]) for line in lines]}
+    rec = [str(line[0]) for line in lines]
     cursor.close()
     return rec
 
@@ -92,5 +92,74 @@ def get_buy_items_of_mulitple_users_by_userid(uidlist):
     cursor.execute(q)
     lines = cursor.fetchall()
     rec = [line[0] for line in lines]
+    cursor.close()
+    return rec
+
+def get_buy_item_categories_of_mulitple_users_by_userid(uidlist):
+    cursor = CONN.cursor()
+    if not isinstance(uidlist, list):
+        raise ValueError('uidlist shold be list')
+    int_uidlist = [str(uid) for uid in uidlist]
+    q = 'select distinct item_category from userlist where user_id in (%s) and behavior_type=4' % ','.join(int_uidlist)
+    cursor.execute(q)
+    lines = cursor.fetchall()
+    rec = [line[0] for line in lines]
+    cursor.close()
+    return rec
+
+def get_user_geo_by_uid(uid):
+    cursor = CONN.cursor()
+    int_uid = uid
+    if not isinstance(uid, int):
+        int_uid = int(int_uid)
+    cursor.execute('select distinct user_geohash from userlist where user_id=%s' % int_uid)
+    line = cursor.fetchone()
+    rec = {uid: str(line[0])}
+    cursor.close()
+    return rec
+
+def get_buy_uid():
+    cursor = CONN.cursor()
+    cursor.execute('select distinct user_id from userlist where behavior_type=4')
+    lines = cursor.fetchall()
+    rec = [line[0] for line in lines]
+    cursor.close()
+    return rec
+
+def get_user_buy_item_categories_by_userid(uid):
+    cursor = CONN.cursor()
+    int_uid = uid
+    if not isinstance(uid, int):
+        int_uid = int(int_uid)
+    cursor.execute('select distinct item_category from userlist where user_id=%s and behavior_type=4' % int_uid)
+    lines = cursor.fetchall()
+    rec = {uid: [str(line[0]) for line in lines]}
+    cursor.close()
+    return rec
+
+def get_item_category_count_in_userlist():
+    cursor = CONN.cursor()
+    cursor.execute('select count(*) from userlist where behavior_type=4 group by item_category')
+    lines = cursor.fetchall()
+    rec = [str(line[0]) for line in lines]
+    cursor.close()
+    return rec
+
+def get_all_user_selected():
+    cursor = CONN.cursor()
+    cursor.execute('select distinct user_id, count(*) from userlist group by user_id having count(*) >= 2 and behavior_type=4')
+    lines = cursor.fetchall()
+    rec = [line[0] for line in lines]
+    cursor.close()
+    return rec
+
+def get_item_buy_times(tid):
+    cursor = CONN.cursor()
+    int_tid = tid
+    if not isinstance(tid, int):
+        int_tid = int(int_tid)
+    cursor.execute('select count(*) from userlist group by item_id having behavior_type=4 and user_id=%s' % int_tid)
+    lines = cursor.fetchall()
+    rec = [int(line[0]) for line in lines]
     cursor.close()
     return rec
