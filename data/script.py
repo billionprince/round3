5ace57ee20beea642979d20e6ac8de898b206f2b
HC <billionprince@gmail.com>
2015-04-06 21:16:39 +0800

    create test data set to testdata table

--- data/script.py
+++ data/script.py
@@ -4,11 +4,17 @@ import os
 import codecs
 
 def create_table(conn):
-    cursor = conn.cursor()
-    cursor.execute('create table userlist (user_id INTEGER, item_id INTEGER, behavior_type INTEGER, user_geohash text, item_category INTEGER, time text)')
-    cursor.execute('create table itemlist (item_id INTEGER, item_geohash text, item_category INTEGER)')
-    conn.commit()
+    rec = False
+    try:
+        cursor = conn.cursor()
+        cursor.execute('create table userlist (user_id INTEGER, item_id INTEGER, behavior_type INTEGER, user_geohash text, item_category INTEGER, time text)')
+        cursor.execute('create table itemlist (item_id INTEGER, item_geohash text, item_category INTEGER)')
+        conn.commit()
+        rec = True
+    except:
+        pass
     cursor.close()
+    return rec
 
 def readfile(path):
     path = os.path.join(os.path.dirname(settings.__file__), path)
@@ -30,13 +36,28 @@ def insert_data(conn):
     cursor.executemany('INSERT INTO itemlist VALUES (?,?,?)', lines)
     print 'insert data to itemlist done'
     conn.commit()
+    cursor.close()
+
+def create_test_data(conn):
+    cursor = conn.cursor()
+    try:
+        cursor.execute('create table testdata (user_id INTEGER, item_id INTEGER)')
+        conn.commit()
+    except:
+        pass
+    from util import userHandler
+    lines = userHandler.get_user_data_set_by_time(-0.2)
+    cursor.executemany('INSERT INTO testdata VALUES (?,?)', [[line['user_id'], line['item_id']] for line in lines])
+    conn.commit()
+    cursor.close()
 
 
 if __name__ == '__main__':
     try:
         db_path = os.path.join(os.path.dirname(settings.__file__), settings.DB_NAME)
         conn = sqlite3.connect(db_path)
-        create_table(conn)
-        insert_data(conn)
+        if create_table(conn):
+            insert_data(conn)
+        create_test_data(conn)
     except Exception as e:
         print e
