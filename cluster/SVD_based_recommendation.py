70492e025dfab5a685c96264ab373c23f0f7ee4d
Gilhirith <gilhirith@gmail.com>
2015-04-10 11:34:55 +0800

    add category similarity based recommendation

--- cluster/SVD_based_recommendation.py
+++ cluster/SVD_based_recommendation.py
@@ -4,39 +4,72 @@ from recsys.datamodel.data import Data
 import time
 
 def preprocess(op_history_all_user):
-    op_weight = [0, 0, 0, 0, 2]
+    op_weight = [0, 0, 1, 1, 1]
     user_idx = {} # int, map from original id
     item_idx = {} # int, map from original id
+    cat_idx = {}
     user_str = {} # str, original id of each mapped idx
     item_str = {} # str, original id of each mapped idx
+    cat_str = {}
     user_op_item_cnt = {} # operation count [behavior=2,3,4] for each user on each item
+    user_op_cat_cnt = {} # operation count [behavior=2,3,4] for each user on each cat
     item_op_users = {} # operation count [behavior=2,3,4] on each item by all user
+    cat_op_users = {} # operation count [behavior=2,3,4] on each cat by all user
     user_op_cnt = {} # operation count [behavior=2,3,4] for each user
+    cat_item_cnt = {}
     n_user = 1
     n_item = 1
+    n_cat = 1
 
     for hi in op_history_all_user:
+        # print hi
+        # user info
         if hi[0] not in user_op_item_cnt:
             user_idx[hi[0]] = n_user
             user_str[n_user] = hi[0]
             n_user += 1
-            user_op_cnt[hi[0]] = op_weight[hi[2]]
+            user_op_cnt[hi[0]] = op_weight[hi[3]]
             user_op_item_cnt[hi[0]] = {}
+            user_op_cat_cnt[hi[0]] = {}
         else:
-            user_op_cnt[hi[0]] += op_weight[hi[2]]
+            user_op_cnt[hi[0]] += op_weight[hi[3]]
+        # item info
         if hi[1] not in item_op_users:
             item_idx[hi[1]] = n_item
             item_str[n_item] = hi[1]
             n_item += 1
-            item_op_users[hi[1]] = op_weight[hi[2]]
+            item_op_users[hi[1]] = op_weight[hi[3]]
         else:
-            item_op_users[hi[1]] += op_weight[hi[2]]
+            item_op_users[hi[1]] += op_weight[hi[3]]
+        # category info
+        if hi[2] not in cat_op_users:
+            cat_idx[hi[2]] = n_cat
+            cat_str[n_cat] = hi[2]
+            n_cat += 1
+            cat_op_users[hi[2]] = op_weight[hi[3]]
+            cat_item_cnt[hi[2]] = {}
+        else:
+            cat_op_users[hi[2]] += op_weight[hi[3]]
+        # user-item cnt
         if hi[1] not in user_op_item_cnt[hi[0]]:
-            user_op_item_cnt[hi[0]][hi[1]] = op_weight[hi[2]]
+            user_op_item_cnt[hi[0]][hi[1]] = op_weight[hi[3]]
         else:
-            user_op_item_cnt[hi[0]][hi[1]] += op_weight[hi[2]]
+            user_op_item_cnt[hi[0]][hi[1]] += op_weight[hi[3]]
+        # cat-item cnt
+        if hi[1] not in cat_item_cnt[hi[2]]:
+            cat_item_cnt[hi[2]][hi[1]] = op_weight[hi[3]]
+        else:
+            cat_item_cnt[hi[2]][hi[1]] += op_weight[hi[3]]
+        # user-cat cnt
+        if hi[2] not in user_op_cat_cnt[hi[0]]:
+            user_op_cat_cnt[hi[0]][hi[2]] = op_weight[hi[3]]
+        else:
+            user_op_cat_cnt[hi[0]][hi[2]] += op_weight[hi[3]]
 
-    return user_idx, item_idx, user_str, item_str, item_op_users, user_op_cnt, user_op_item_cnt
+    for ci in cat_item_cnt:
+        cat_item_cnt[ci] = sorted(cat_item_cnt[ci].items(), lambda x, y: cmp(x[1], y[1]), reverse = True)
+
+    return user_idx, item_idx, cat_idx, user_str, item_str, cat_str, item_op_users, cat_op_users, user_op_cnt, user_op_item_cnt, user_op_cat_cnt, cat_item_cnt
 
 def build_svd_user_based(user_op_item_cnt, item_op_users, user_idx, item_idx, min_nonzero):
     svd = SVD()
@@ -76,6 +109,44 @@ def build_svd_item_based(user_op_item_cnt, item_op_users, user_idx, item_idx, mi
     svd.set_data(data)
     return svd, item_lst
 
+def build_svd_user_cat_based(user_op_cat_cnt, cat_op_users, user_idx, cat_idx, min_nonzero):
+    svd = SVD()
+    data = Data()
+    user_lst = []
+    for ui in user_op_cat_cnt:
+        if len(user_op_cat_cnt[ui]) < min_nonzero:
+            continue
+        for ci in user_op_cat_cnt[ui]:
+            if cat_op_users[ci] < min_nonzero:
+                continue
+            if 1.0*user_op_cat_cnt[ui][ci] < 1:
+                continue
+            user_lst.append(ui)
+            data.add_tuple(((1.0*user_op_cat_cnt[ui][ci]), user_idx[ui], cat_idx[ci]))
+    user_lst = list(set(user_lst))
+    print 'user =', len(user_lst)
+    svd.set_data(data)
+    return svd, user_lst
+
+def build_svd_cat_based(user_op_cat_cnt, cat_op_users, user_idx, cat_idx, min_nonzero):
+    svd = SVD()
+    data = Data()
+    cat_lst = []
+    for ui in user_op_cat_cnt:
+        if len(user_op_cat_cnt[ui]) < min_nonzero:
+            continue
+        for ci in user_op_cat_cnt[ui]:
+            if cat_op_users[ci] < min_nonzero:
+                continue
+            if 1.0*user_op_cat_cnt[ui][ci] < 1:
+                continue
+            cat_lst.append(ci)
+            data.add_tuple(((1.0*user_op_cat_cnt[ui][ci]), cat_idx[ci], user_idx[ui]))
+    cat_lst = list(set(cat_lst))
+    print 'cat =', len(cat_lst)
+    svd.set_data(data)
+    return svd, cat_lst
+
 def svd_solver(svd, num_sigular):
     svd.compute(k=num_sigular,
                 min_values=1, # remove lines full of zero
@@ -83,6 +154,7 @@ def svd_solver(svd, num_sigular):
                 mean_center=False,
                 post_normalize=True,
                 savefile='svd_res')
+    # print svd.get_matrix()
     return svd
 
 def svd_user_similarity(svd, user_lst, user_idx, user_str, num_sim, sim_thr):
@@ -91,6 +163,8 @@ def svd_user_similarity(svd, user_lst, user_idx, user_str, num_sim, sim_thr):
         sim_user[ui] = []
         tp_sim = svd.similar(int(user_idx[ui]), num_sim)
         for uj in tp_sim:
+            if user_str[uj[0]] == ui:
+                continue
             if uj[1] > sim_thr:
                 # print uj[1]
                 sim_user[ui].append(user_str[uj[0]])
@@ -107,15 +181,57 @@ def svd_item_similarity(svd, item_lst, item_idx, item_str, num_sim, sim_thr):
                 sim_item[ti].append(item_str[tj[0]])
     return sim_item
 
-def recommendation_by_sim_user(recommendation_dict, sim_user, user_op_item_cnt, user_op_cnt, op_ratio_thr):
+def svd_user_cat_similarity(svd, user_lst, user_idx, user_str, num_sim, sim_thr):
+    sim_user = {}
+    for ui in user_lst:
+        sim_user[ui] = []
+        tp_sim = svd.similar(int(user_idx[ui]), num_sim)
+        # print tp_sim
+        for uj in tp_sim:
+            if user_str[uj[0]] == ui:
+                continue
+            # print uj[1]
+            if uj[1] > sim_thr:
+                # print uj[1]
+                sim_user[ui].append(user_str[uj[0]])
+        # print sim_user[ui]
+    return sim_user
+
+def svd_cat_similarity(svd, cat_lst, cat_idx, cat_str, num_sim, sim_thr):
+    sim_cat = {}
+    for ti in cat_lst:
+        sim_cat[ti] = []
+        tp_sim = svd.similar(int(cat_idx[ti]), num_sim)
+        for tj in tp_sim:
+            # print tj[1]
+            if tj[1] > sim_thr:
+                sim_cat[ti].append(cat_str[tj[0]])
+    return sim_cat
+
+def recommendation_by_sim_user(recommendation_dict, user_buy_item, sim_user, user_op_item_cnt, user_op_cnt, op_ratio_thr):
+    # print 'thr=', op_ratio_thr
+    # for ui in sim_user:
+    #     if ui not in recommendation_dict:
+    #         recommendation_dict[ui] = []
+    #     for uj in sim_user[ui]:
+    #         for ti in user_op_item_cnt[uj]:
+    #             # print user_op_item_cnt[uj][ti]
+    #             # print user_op_cnt[uj]
+    #             if 1.0*user_op_item_cnt[uj][ti]/(1.0*user_op_cnt[uj]) > op_ratio_thr:
+    #                 # print 'here'
+    #                 recommendation_dict[ui].append(ti)
+    #         recommendation_dict[ui] = list(set(recommendation_dict[ui]))
+    # return recommendation_dict
     for ui in sim_user:
         if ui not in recommendation_dict:
             recommendation_dict[ui] = []
         for uj in sim_user[ui]:
-            for ti in user_op_item_cnt[uj]:
-                if 1.0*user_op_item_cnt[uj][ti]/(1.0*user_op_cnt[uj]) > op_ratio_thr:
-                    recommendation_dict[ui].append(ti)
+            if uj not in user_buy_item:
+                continue
+            for ti in user_buy_item[uj]:
+                recommendation_dict[ui].append(ti)
             recommendation_dict[ui] = list(set(recommendation_dict[ui]))
+        print recommendation_dict[ui]
     return recommendation_dict
 
 def recommendation_by_sim_item(recommendation_dict, sim_item, user_op_item_cnt, item_op_users, user_buy_item, item_op_thr):
@@ -131,6 +247,40 @@ def recommendation_by_sim_item(recommendation_dict, sim_item, user_op_item_cnt, 
             recommendation_dict[ui] = list(set(recommendation_dict[ui]))
     return recommendation_dict
 
+def recommendation_by_sim_user_cat(recommendation_dict, user_buy_item, sim_user_cat, user_op_item_cnt, user_op_cnt, op_ratio_thr):
+    # print 'thr=', op_ratio_thr
+    for ui in sim_user_cat:
+        if ui not in recommendation_dict:
+            recommendation_dict[ui] = []
+        for uj in sim_user_cat[ui]:
+            if uj not in user_buy_item:
+                continue
+            for ti in user_buy_item[uj]:
+                # print user_op_item_cnt[uj][ti]
+                # print user_op_cnt[uj]
+                # if 1.0*user_op_item_cnt[uj][ti]/(1.0*user_op_cnt[uj]) > op_ratio_thr:
+                    # print 'here'
+                recommendation_dict[ui].append(ti)
+            recommendation_dict[ui] = list(set(recommendation_dict[ui]))
+        print recommendation_dict[ui]
+    return recommendation_dict
+
+def recommendation_by_sim_cat(recommendation_dict, sim_cat, user_op_cat_cnt, user_buy_cat, cat_op_thr, cat_op_users, cat_item_cnt, min_cat_buy):
+    for ui in user_buy_cat:
+        if ui not in recommendation_dict:
+            recommendation_dict[ui] = []
+        for ci in user_buy_cat[ui]:
+            if ci not in sim_cat:
+                continue
+            for cj in sim_cat[ci]:
+                if cat_op_users[cj] > cat_op_thr:
+                    for ti in cat_item_cnt[cj]:
+                        if ti[1] < min_cat_buy:
+                            break
+                        recommendation_dict[ui].append(ti[0])
+        recommendation_dict[ui] = list(set(recommendation_dict[ui]))
+    return recommendation_dict
+
 def recommendation_by_self_op_history(recommendation_dict, user_buy_item, user_op_item_cnt, user_op_cnt, min_user_op, op_ratio_thr):
     for ui in user_buy_item:
         if user_op_cnt[ui] < min_user_op:
@@ -143,21 +293,20 @@ def recommendation_by_self_op_history(recommendation_dict, user_buy_item, user_o
         recommendation_dict[ui] = list(set(recommendation_dict[ui]))
     return recommendation_dict
 
-def recommendation(num_sigular, min_nonzero, num_sim_user, num_sim_item, sim_thr, op_ratio_thr, item_op_thr, min_user_op):
+def recommendation(num_sigular, min_nonzero, num_sim_user, num_sim_item, num_sim_cat, sim_thr, min_user_op, op_ratio_thr, item_op_thr, cat_op_thr, min_cat_buy):
     t0=time.clock()
 
-    op_history_all_user = userHandler.get_all_userid_and_itemid_and_behavior_type_no_distinct('traindataselecttotal') #  traindataselect
-    user_buy_item = userHandler.get_all_userid_and_itemid_distinct(4, 'databuytotal') #  trainbuydata
-    cnt = 0
-    for ui in user_buy_item:
-        cnt += len(user_buy_item[ui])
-    print 'cnt=', cnt
+    op_history_all_user = userHandler.get_all_userid_and_itemid_and_behavior_type_no_distinct('traindataselect') #   traindataselecttotal
+    # user_buy_item = userHandler.get_all_userid_and_itemid_distinct(4, 'trainbuydata') # databuytotal
+    user_buy_cat = userHandler.get_all_userid_and_itemcat_distinct(4, 'trainbuydata')
+    # item_cat = userHandler.get_all_itemid_and_itemcat('traindata')
+
     t1 = time.clock()
     print 'sql operation time:', t1 - t0
 
     recommendation_dict = {}
 
-    user_idx, item_idx, user_str, item_str, item_op_users, user_op_cnt, user_op_item_cnt = preprocess(op_history_all_user)
+    user_idx, item_idx, cat_idx, user_str, item_str, cat_str, item_op_users, cat_op_users, user_op_cnt, user_op_item_cnt, user_op_cat_cnt, cat_item_cnt = preprocess(op_history_all_user)
 
     t2 = time.clock()
     print 'preprocess data time:', t2 - t1
@@ -168,7 +317,7 @@ def recommendation(num_sigular, min_nonzero, num_sim_user, num_sim_item, sim_thr
 
     sim_user = svd_user_similarity(svd_user_based, user_lst, user_idx, user_str, num_sim_user, sim_thr)
 
-    recommendation_dict = recommendation_by_sim_user(recommendation_dict, sim_user, user_op_item_cnt, user_op_cnt, op_ratio_thr)
+    recommendation_dict = recommendation_by_sim_user(recommendation_dict, user_buy_item, sim_user, user_op_item_cnt, user_op_cnt, op_ratio_thr)
     n_recomm_sim_user = 0
     for ui in recommendation_dict:
         n_recomm_sim_user += len(recommendation_dict[ui])
@@ -196,10 +345,34 @@ def recommendation(num_sigular, min_nonzero, num_sim_user, num_sim_item, sim_thr
     n_recomm_self_op = 0
     for ui in recommendation_dict:
         n_recomm_self_op += len(recommendation_dict[ui])
-    print 'recommendation by self operation:', n_recomm_self_op# - n_recomm_sim_item
+    print 'recommendation by self operation:', n_recomm_self_op - n_recomm_sim_item
 
     t5 = time.clock()
     print 'recommendation by self operation time:', t5 - t4
+
+    svd_user_cat, user_lst = build_svd_user_cat_based(user_op_cat_cnt, cat_op_users, user_idx, cat_idx, min_nonzero)
+
+    svd_user_cat = svd_solver(svd_user_cat, num_sigular)
+
+    sim_user_cat = svd_user_cat_similarity(svd_user_cat, user_lst, user_idx, user_str, num_sim_user, sim_thr)
+
+    recommendation_dict = recommendation_by_sim_user_cat(recommendation_dict, user_buy_item, sim_user_cat, user_op_item_cnt, user_op_cnt, op_ratio_thr)
+    n_recomm_user_cat = 0
+    for ui in recommendation_dict:
+        n_recomm_user_cat += len(recommendation_dict[ui])
+    print 'recommendation by user-cat similarity:', n_recomm_user_cat
+
+    svd_cat_based, cat_lst = build_svd_cat_based(user_op_cat_cnt, cat_op_users, user_idx, cat_idx, min_nonzero)
+
+    svd_cat_based = svd_solver(svd_cat_based, num_sigular)
+
+    sim_cat = svd_cat_similarity(svd_cat_based, cat_lst, cat_idx, cat_str, num_sim_cat, sim_thr)
+
+    recommendation_dict = recommendation_by_sim_cat(recommendation_dict, sim_cat, user_op_cat_cnt, user_buy_cat, cat_op_thr, cat_op_users, cat_item_cnt, min_cat_buy)
+    n_recomm_sim_item = 0
+    for ui in recommendation_dict:
+        n_recomm_sim_item += len(recommendation_dict[ui])
+    print 'recommendation by item similarity:', n_recomm_sim_item - n_recomm_sim_user
 
     return recommendation_dict
 
