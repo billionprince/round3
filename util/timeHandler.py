11b9c0acf396a027e080d22948d1a43fefc923bb
HC <bill.huang@dianping.com>
2015-04-11 23:21:29 +0800

    add timeHandler function

---
+++ util/timeHandler.py
@@ -0,0 +1,8 @@
+import datetime
+
+def sub_days_of_two_time(time1, time2):
+    if not isinstance(time1, basestring) or not isinstance(time2, basestring):
+        raise Exception('parameters of function "sub_days_of_two_time" show be string')
+    d1 = datetime.datetime.strptime(time1.split(' ')[0], '%Y-%m-%d')
+    d2 = datetime.datetime.strptime(time2.split(' ')[0], '%Y-%m-%d')
+    return (d2 - d1).days
