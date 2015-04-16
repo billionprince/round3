476494d48ef1fea33fb091e451964d3432966fcb
Gilhirith <gilhirith@gmail.com>
2015-04-16 14:12:36 +0800

    add init function

--- cluster/user_behavior_based_recommendation.py
+++ cluster/user_behavior_based_recommendation.py
@@ -1,13 +1,17 @@
 from util import timeHandler, dbHandler
 
 
-TRAIN_DELETE_NOISY_DATA = 'userlistwithoutnoisy' #traindeletenoisydata traindata
+TRAIN_DELETE_NOISY_DATA = 'traindata' #traindeletenoisydata  userlistwithoutnoisy
 TRAIN_SCAN_TABLE = 'trainscandata'
 TRAIN_CART_TABLE = 'traincartdata'
 TRAIN_BUY_TABLE = 'trainbuydata'
 USERLIST_WITHOUT_NOISY_TABLE_NAME = 'userlistwithoutnoisy'
-USER_BEHAV_COUNT_TABLE_LAST_DAYS = 'user_behavior_cnt_last_total' #user_behavior_cnt_last
-TRAIN_USER_BEHAV_CNT_TIME_TABLE = 'totalcnttime' #traincnttime
+USER_BEHAV_COUNT_TABLE_LAST_DAYS = 'user_behavior_cnt_last' # user_behavior_cnt_last_total
+TRAIN_USER_BEHAV_CNT_TIME_TABLE = 'traincnttime' #'totalcnttime'
+SELECTED_USER_BEHAV_CNT_TIME_TABLE = 'selectedusercnttime'
+TARGET_USER_TABLE = 'target_user'
+TEST_END_TIME = '2014-12-14 00'
+TOTAL_END_TIME = '2014-12-16 23'
 
 USERLIST = ['user_id', 'item_id', 'behavior_type', 'user_geohash', 'item_category', 'time']
 IMTELIST = ['item_id', 'item_geohash', 'item_category']
@@ -46,98 +50,79 @@ def user_buy_time_interval_distribution(user_behav_cnt_time, max_day_interv):
     return user_behav_distri, user_buy_ratio, item_buy_ratio
 
 
-def user_beavior_based_recommendation(user_behav_distri, user_buy_ratio, item_buy_ratio, user_behav_last_days, min_possi_ratio, min_buy_ratio):
+def user_beavior_based_recommendation(user_lst, user_behav_distri, user_buy_ratio, item_buy_ratio, user_behav_last_days, end_time, min_possi_ratio, min_buy_ratio):
     recommendation_dict = {}
     for ti in user_behav_last_days:
-        if ti[0] not in user_behav_distri or ti[2] == '4':
+        if ti[0] not in user_behav_distri or ti[2] == '4' or ti[0] not in user_lst:
             continue
         recommendation_dict[ti[0]] = []
-        days = timeHandler.sub_days_of_two_time(ti[5], '2014-12-16 23')
+        days = timeHandler.sub_days_of_two_time(ti[5], end_time)
         user_interv_possibility = sum(user_behav_distri[ti[0]][0:days+1])
-        if user_interv_possibility > min_possi_ratio and user_buy_ratio[ti[0]]> min_buy_ratio and item_buy_ratio[ti[1]] > min_buy_ratio:
+        if user_interv_possibility > min_possi_ratio and user_buy_ratio[ti[0]] > min_buy_ratio and item_buy_ratio[ti[1]] > 0:
             recommendation_dict[ti[0]].append(ti[1])
-    cnt = 0
+    # cnt = 0
     for ui in recommendation_dict:
         if recommendation_dict[ui]:
             recommendation_dict[ui] = list(set(recommendation_dict[ui]))
-            cnt += len(recommendation_dict[ui])
-    print cnt
+            # cnt += len(recommendation_dict[ui])
+    # print cnt
     return recommendation_dict
 
 
-def recommendation(max_day_interv, min_possi_ratio, min_buy_ratio):
-
-    user_behav_cnt_time = dbHandler.read_table(TRAIN_USER_BEHAV_CNT_TIME_TABLE)
-
+def recommendation(max_day_interv, min_possi_ratio, min_buy_ratio, end_time, flush=False, isTest=True):
+    init(flush, isTest, end_time)
+    user_lst = dbHandler.read_table(TARGET_USER_TABLE)#(TARGET_USER_TABLE)
+    print 'num of selected user:', len(user_lst)
+    for ui in user_lst:
+        print ui
+    print user_lst
+    user_behav_cnt_time = dbHandler.read_table(SELECTED_USER_BEHAV_CNT_TIME_TABLE)
     user_behav_distri, user_buy_ratio, item_buy_ratio = user_buy_time_interval_distribution(user_behav_cnt_time, max_day_interv)
-    print 'end1'
     user_behav_last_days = dbHandler.read_table(USER_BEHAV_COUNT_TABLE_LAST_DAYS)
-    print 'end2'
-    recommendation_dict = user_beavior_based_recommendation(user_behav_distri, user_buy_ratio, item_buy_ratio, user_behav_last_days, min_possi_ratio, min_buy_ratio)
-    print 'end3'
+    recommendation_dict = user_beavior_based_recommendation(user_lst, user_behav_distri, user_buy_ratio, item_buy_ratio, user_behav_last_days, end_time, min_possi_ratio, min_buy_ratio)
     return recommendation_dict
 
-
-def create_table_user_behavior_last_days(tableName_input, tableName_output, time):
-    cursor = dbHandler.CONN.cursor()
-    q = 'select * from %s where time > "%s"' % (tableName_input, time)
-    cursor.execute(q)
-    lines = cursor.fetchall()
-    dbHandler.create_table(tableName_output, USERLIST, lines, True)
-    cursor.close()
-
-
-def user_behavior_count_and_time_distinct(tableName_input, tableName_output):
-    cursor = dbHandler.CONN.cursor()
-    q = 'select user_id, item_id, min(time), count(*) as num_scan from %s ' % tableName_input
-    q += 'where behavior_type=1 group by user_id, item_id'
-    cursor.execute(q)
-    data = {(line[0], line[1]): [str(line[2]), line[3], 0, 0, 0, 0] for line in cursor.fetchall()}
-    q = 'select user_id, item_id, min(time), count(*) as num_cart from %s ' % tableName_input
-    q += 'where behavior_type=3 group by user_id, item_id'
-    cursor.execute(q)
-    for line in cursor.fetchall():
-        key = (line[0], line[1])
-        if key not in data:
-            data[key] = [0, 0, str(line[2]), line[3], 0, 0]
-        else:
-            data[key][2] = str(line[2])
-            data[key][3] = line[3]
-    q = 'select user_id, item_id, min(time), count(*) as num_buy from %s ' % tableName_input
-    q += 'where behavior_type=4 group by user_id, item_id'
-    cursor.execute(q)
-    for line in cursor.fetchall():
-        key = (line[0], line[1])
-        if key not in data:
-            data[key] = [0, 0, 0, 0, str(line[2]), line[3]]
-        else:
-            data[key][4] = str(line[2])
-            data[key][5] = line[3]
-    title = ['user_id', 'item_id', 'initial_scan_time', 'num_scan', 'initial_cart_time', 'num_cart', 'initial_buy_time', 'num_buy']
-    lines = [list(k) + v for k, v in data.iteritems()]
-    dbHandler.create_table(tableName_output, title, lines, True)
-    dbHandler.CONN.commit()
-    # cursor.close()
-
- # BEGIN Preprocess --- create table
-    # user_behavior_count_and_time_distinct(TRAIN_DELETE_NOISY_DATA, TRAIN_USER_BEHAV_CNT_TIME_TABLE)
-    # create_table_user_behavior_last_days(TRAIN_DELETE_NOISY_DATA, USER_BEHAV_COUNT_TABLE_LAST_DAYS, '2014-12-14 00')
-    # END Preprocess --- create table
-
-def init(tableName_input, tableName_output, flush=False):
+def init(flush=False, isTest=True, end_time=TEST_END_TIME):
+    if not isTest: #prepare to submit
+        flush = True
+        TRAIN_DELETE_NOISY_DATA = 'userlistwithoutnoisy'
+        end_time = TOTAL_END_TIME
+    else:
+        TRAIN_DELETE_NOISY_DATA = 'traindata'
     if flush:
-        if dbHandler.table_exist(TRAIN_USER_BEHAV_CNT_TIME_TABLE):
-            dbHandler.delete_table(TRAIN_USER_BEHAV_CNT_TIME_TABLE)
+        if dbHandler.table_exist(TARGET_USER_TABLE):
+            dbHandler.delete_table(TARGET_USER_TABLE)
         if dbHandler.table_exist(USER_BEHAV_COUNT_TABLE_LAST_DAYS):
             dbHandler.delete_table(USER_BEHAV_COUNT_TABLE_LAST_DAYS)
-    if not dbHandler.table_exist(TRAIN_USER_BEHAV_CNT_TIME_TABLE):
+        if dbHandler.table_exist(SELECTED_USER_BEHAV_CNT_TIME_TABLE):
+            dbHandler.delete_table(SELECTED_USER_BEHAV_CNT_TIME_TABLE)
+    if not dbHandler.table_exist(TARGET_USER_TABLE):
         cursor = dbHandler.CONN.cursor()
-        q = 'select user_id, item_id, min(time), count(*) as num_scan from %s ' % tableName_input
-        q += 'where behavior_type=1 group by user_id, item_id'
+        print TRAIN_DELETE_NOISY_DATA
+        q = 'select distinct user_id, count(*) from %s where behavior_type=4 group by user_id having count(*)>=2' % (TRAIN_DELETE_NOISY_DATA)
+        cursor.execute(q)
+        lines = cursor.fetchall()
+        dbHandler.create_table(TARGET_USER_TABLE, ['user_id', 'num_buy'], lines, True)
+        cursor.close()
+    if not dbHandler.table_exist(SELECTED_USER_BEHAV_CNT_TIME_TABLE):
+        cursor = dbHandler.CONN.cursor()
+        q = 'select a.user_id, a.item_id, a.min_time, a.num_scan from '
+        q += '(select user_id, item_id, min(time) as min_time, count(*) as num_scan from %s ' % TRAIN_DELETE_NOISY_DATA
+        q += 'where behavior_type=1 group by user_id, item_id) a '
+        q += 'join %s b on a.user_id=b.user_id' % TARGET_USER_TABLE
         cursor.execute(q)
         data = {(line[0], line[1]): [str(line[2]), line[3], 0, 0, 0, 0] for line in cursor.fetchall()}
-        q = 'select user_id, item_id, min(time), count(*) as num_cart from %s ' % tableName_input
-        q += 'where behavior_type=3 group by user_id, item_id'
+        for line in cursor.fetchall():
+            key = (line[0], line[1])
+            if key not in data:
+                data[key] = [0, 0, str(line[2]), line[3], 0, 0]
+            else:
+                data[key][2] = str(line[2])
+                data[key][3] = line[3]
+        q = 'select a.user_id, a.item_id, a.min_time, a.num_cart from '
+        q += '(select user_id, item_id, min(time) as min_time, count(*) as num_cart from %s ' % TRAIN_DELETE_NOISY_DATA
+        q += 'where behavior_type=3 group by user_id, item_id) a '
+        q += 'join %s b on a.user_id=b.user_id' % TARGET_USER_TABLE
         cursor.execute(q)
         for line in cursor.fetchall():
             key = (line[0], line[1])
@@ -146,8 +131,10 @@ def init(tableName_input, tableName_output, flush=False):
             else:
                 data[key][2] = str(line[2])
                 data[key][3] = line[3]
-        q = 'select user_id, item_id, min(time), count(*) as num_buy from %s ' % tableName_input
-        q += 'where behavior_type=4 group by user_id, item_id'
+        q = 'select a.user_id, a.item_id, a.min_time, a.num_buy from '
+        q += '(select user_id, item_id, min(time) as min_time, count(*) as num_buy from %s ' % TRAIN_DELETE_NOISY_DATA
+        q += 'where behavior_type=4 group by user_id, item_id) a '
+        q += 'join %s b on a.user_id=b.user_id' % TARGET_USER_TABLE
         cursor.execute(q)
         for line in cursor.fetchall():
             key = (line[0], line[1])
@@ -158,16 +145,26 @@ def init(tableName_input, tableName_output, flush=False):
                 data[key][5] = line[3]
         title = ['user_id', 'item_id', 'initial_scan_time', 'num_scan', 'initial_cart_time', 'num_cart', 'initial_buy_time', 'num_buy']
         lines = [list(k) + v for k, v in data.iteritems()]
-        dbHandler.create_table(tableName_output, title, lines, True)
-        dbHandler.CONN.commit()
+        dbHandler.create_table(SELECTED_USER_BEHAV_CNT_TIME_TABLE, title, lines, True)
+        cursor.close()
     if not dbHandler.table_exist(USER_BEHAV_COUNT_TABLE_LAST_DAYS):
         cursor = dbHandler.CONN.cursor()
-        q = 'select * from %s where time > "%s"' % (tableName_input, time)
+        q = 'select * from %s where time > "%s"' % (TRAIN_DELETE_NOISY_DATA, end_time)
         cursor.execute(q)
         lines = cursor.fetchall()
-        dbHandler.create_table(tableName_output, USERLIST, lines, True)
+        dbHandler.create_table(USER_BEHAV_COUNT_TABLE_LAST_DAYS, USERLIST, lines, True)
         cursor.close()
 
 
 if __name__ == '__main__':
-    recommendation(6, 0.5, 0.3)
+
+    isTest = True
+    flush = True
+    end_time = TEST_END_TIME
+    max_day_interv = 3
+    min_possi_ratio = 0.4
+    min_buy_ratio = 0.2
+
+    reccomendation_dict = recommendation(max_day_interv, min_possi_ratio, min_buy_ratio, end_time, flush, isTest)
+
+# for running test, isTest set to True, for submit, isTest set to False
