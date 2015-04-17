d6bb1be67c05633122af77de629e4b2a83aea4b1
Gilhirith <gilhirith@gmail.com>
2015-04-17 13:12:10 +0800

    edit recommendation strategy

--- cluster/user_behavior_based_recommendation.py
+++ cluster/user_behavior_based_recommendation.py
@@ -1,5 +1,5 @@
-from util import timeHandler, dbHandler
-
+from util import timeHandler, dbHandler, fileHandler
+import settings
 
 TRAIN_DELETE_NOISY_DATA = 'traindata' #traindeletenoisydata  userlistwithoutnoisy
 TRAIN_SCAN_TABLE = 'trainscandata'
@@ -11,7 +11,7 @@ TRAIN_USER_BEHAV_CNT_TIME_TABLE = 'traincnttime' #'totalcnttime'
 SELECTED_USER_BEHAV_CNT_TIME_TABLE = 'selectedusercnttime'
 TARGET_USER_TABLE = 'target_user'
 TEST_END_TIME = '2014-12-14 00'
-TOTAL_END_TIME = '2014-12-16 23'
+TOTAL_END_TIME = '2014-12-15 23'
 
 USERLIST = ['user_id', 'item_id', 'behavior_type', 'user_geohash', 'item_category', 'time']
 IMTELIST = ['item_id', 'item_geohash', 'item_category']
@@ -50,36 +50,43 @@ def user_buy_time_interval_distribution(user_behav_cnt_time, max_day_interv):
     return user_behav_distri, user_buy_ratio, item_buy_ratio
 
 
-def user_beavior_based_recommendation(user_lst, user_behav_distri, user_buy_ratio, item_buy_ratio, user_behav_last_days, end_time, min_possi_ratio, min_buy_ratio):
+def user_beavior_based_recommendation(user_behav_cnt_time, user_behav_distri, user_buy_ratio, item_buy_ratio, user_behav_last_days, end_time, min_possi_ratio, min_buy_ratio):
     recommendation_dict = {}
     for ti in user_behav_last_days:
-        if ti[0] not in user_behav_distri or ti[2] == '4' or ti[0] not in user_lst:
-            continue
         recommendation_dict[ti[0]] = []
-        days = timeHandler.sub_days_of_two_time(ti[5], end_time)
-        user_interv_possibility = sum(user_behav_distri[ti[0]][0:days+1])
-        if user_interv_possibility > min_possi_ratio and user_buy_ratio[ti[0]] > min_buy_ratio and item_buy_ratio[ti[1]] > 0:
-            recommendation_dict[ti[0]].append(ti[1])
-    # cnt = 0
+        if ti[0] not in user_behav_distri or ti[1] not in item_buy_ratio:
+            continue
+        if ti[2] == '4':
+            pass
+            # tp = [i for i in user_behav_cnt_time if ti[0]==i[0] and ti[1]==i[1]]
+            # print 'tp=', tp
+            # if tp:
+            #     if (tp[0][4] and timeHandler.sub_days_of_two_time(tp[0][4], ti[5]) < 0) or (tp[0][2] and timeHandler.sub_days_of_two_time(tp[0][2], ti[5]) < 0):
+            #         recommendation_dict[ti[0]].append(ti[1])
+        else:
+            days = timeHandler.sub_days_of_two_time(ti[5], end_time)
+            user_interv_possibility = sum(user_behav_distri[ti[0]][0:days+1])
+            if user_interv_possibility > min_possi_ratio and user_buy_ratio[ti[0]] > min_buy_ratio and item_buy_ratio[ti[1]] > 0:
+                recommendation_dict[ti[0]].append(ti[1])
+    # cnt = len(recommendation_dict)
+    cnt = 0
     for ui in recommendation_dict:
         if recommendation_dict[ui]:
             recommendation_dict[ui] = list(set(recommendation_dict[ui]))
-            # cnt += len(recommendation_dict[ui])
-    # print cnt
+            cnt += len(recommendation_dict[ui])
+    print 'num of recommendation:', cnt
     return recommendation_dict
 
 
 def recommendation(max_day_interv, min_possi_ratio, min_buy_ratio, end_time, flush=False, isTest=True):
     init(flush, isTest, end_time)
-    user_lst = dbHandler.read_table(TARGET_USER_TABLE)#(TARGET_USER_TABLE)
-    print 'num of selected user:', len(user_lst)
-    for ui in user_lst:
-        print ui
-    print user_lst
+    user_lst =dbHandler.read_table(TARGET_USER_TABLE)
     user_behav_cnt_time = dbHandler.read_table(SELECTED_USER_BEHAV_CNT_TIME_TABLE)
     user_behav_distri, user_buy_ratio, item_buy_ratio = user_buy_time_interval_distribution(user_behav_cnt_time, max_day_interv)
+
     user_behav_last_days = dbHandler.read_table(USER_BEHAV_COUNT_TABLE_LAST_DAYS)
-    recommendation_dict = user_beavior_based_recommendation(user_lst, user_behav_distri, user_buy_ratio, item_buy_ratio, user_behav_last_days, end_time, min_possi_ratio, min_buy_ratio)
+
+    recommendation_dict = user_beavior_based_recommendation(user_behav_cnt_time, user_behav_distri, user_buy_ratio, item_buy_ratio, user_behav_last_days, end_time, min_possi_ratio, min_buy_ratio)
     return recommendation_dict
 
 def init(flush=False, isTest=True, end_time=TEST_END_TIME):
@@ -99,7 +106,7 @@ def init(flush=False, isTest=True, end_time=TEST_END_TIME):
     if not dbHandler.table_exist(TARGET_USER_TABLE):
         cursor = dbHandler.CONN.cursor()
         print TRAIN_DELETE_NOISY_DATA
-        q = 'select distinct user_id, count(*) from %s where behavior_type=4 group by user_id having count(*)>=2' % (TRAIN_DELETE_NOISY_DATA)
+        q = 'select distinct user_id, count(*) from %s where behavior_type=4 group by user_id having count(*)>=3' % (TRAIN_DELETE_NOISY_DATA)
         cursor.execute(q)
         lines = cursor.fetchall()
         dbHandler.create_table(TARGET_USER_TABLE, ['user_id', 'num_buy'], lines, True)
@@ -157,14 +164,13 @@ def init(flush=False, isTest=True, end_time=TEST_END_TIME):
 
 
 if __name__ == '__main__':
-
-    isTest = True
+# for running test, isTest set to True, for submit, isTest set to False, do not change others
+    isTest = False
     flush = True
     end_time = TEST_END_TIME
-    max_day_interv = 3
-    min_possi_ratio = 0.4
-    min_buy_ratio = 0.2
+    max_day_interv = 5
+    min_possi_ratio = 0
+    min_buy_ratio = 0
 
-    reccomendation_dict = recommendation(max_day_interv, min_possi_ratio, min_buy_ratio, end_time, flush, isTest)
-
-# for running test, isTest set to True, for submit, isTest set to False
+    recommendation_dict = recommendation(max_day_interv, min_possi_ratio, min_buy_ratio, end_time, flush, isTest)
+    fileHandler.writeCsvFile(settings.OUTPUT_CSV, recommendation_dict, ['user_id', 'item_id'])
